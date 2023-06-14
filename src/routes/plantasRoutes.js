f0e98cfe5ceae1b73e9b614ac40dba9e76e31bde
thisouza01 <tihsouza01@gmail.com>
2023-06-14 10:21:00 -0300

    refatorando rota de /plantas

--- src/routes/plantasRoutes.js
+++ src/routes/plantasRoutes.js
@@ -1,60 +1,13 @@
 import express from "express";
+import PlantaController from "../controllers/plantasController";
 
 const router = express.Router();
 
 router
-
-    .get('/plantas', (req, res) => {
-        try {
-            res.status(200).send('todas as plantas')
-        } catch (err) {
-            if(err) {
-                res.status(400).send(err)
-            }
-        }
-    })
-
-    .get('/plantas/:id', (req, res) => {
-        try {
-            const id = req.params.id;
-            res.status(200).send(`plantas:${id}`)
-        } catch (err) {
-            if(err) {
-                res.status(400).send(err)
-            }
-        }
-    })
-    
-    .post('/plantas', (req, res) => { 
-        try {
-            res.status(201).send('Planta cadastrada!')
-        } catch (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        }
-    })
-    
-    .put('/plantas/:id', (req, res) => {
-        try {
-            const id = req.params.id;
-            res.status(200).send('Planta atualizada com sucesso! : ', id)
-        } catch (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        }
-    })
-    
-    .delete('/plantas/:id', (req, res) => {
-        try {
-            const id = req.params.id;
-            res.status(200).send('Planta removida com sucesso! : ', id)
-        }catch (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        }
-    })
+    .get('/plantas', PlantaController.listarPlantas)
+    .get('/plantas/:id', PlantaController.listarPlantasPorId)
+    .post('/plantas', PlantaController.cadastrarPlantas)
+    .put('/plantas/:id', PlantaController.atualizarPlantas)
+    .delete('/plantas/:id', PlantaController.excluirPlantas)
 
 export default router;
